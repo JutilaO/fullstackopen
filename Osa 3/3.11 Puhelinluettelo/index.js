113238a8ed92ad917b5598f25572739200ee5714
JutilaO <46874717+JutilaO@users.noreply.github.com>
2023-02-06 11:51:45 +0200

    Add files via upload

---
+++ Osa 3/3.11 Puhelinluettelo/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
+
+let persons = 
+[
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+    },
+    { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+    },
+    { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/info', (req, res) => {
+    const time = new Date()
+    res.send(`<p>Phonebook contains the information of ${persons.length} people</p><p>${time.toString()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body
+    person.id = Math.floor(Math.random() * 9999)
+    if(!person.number || !person.name) {
+        return res.status(400).json({
+            error: "name or number missing"
+        })
+    }
+    if(persons.find(p => p.name === person.name)){
+        return res.status(400).json({
+            error: `${person.name} is already in the phonebook`
+        })
+    }
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
