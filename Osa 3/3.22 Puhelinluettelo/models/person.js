9b6eaa7f37a0941f7054ccb66aea023c6bb066d0
JutilaO <46874717+JutilaO@users.noreply.github.com>
2023-02-08 13:27:52 +0200

    Add files via upload

---
+++ Osa 3/3.22 Puhelinluettelo/models/person.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+function validator(value){
+  return /^\d{2}-{1}/.test(value) || /^\d{3}-{1}/.test(value)
+}
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: [validator, 'Phone number not valid']
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
