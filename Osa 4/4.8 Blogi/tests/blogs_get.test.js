270123648ff929397f75ffe1aefedbc98ef0bd1d
JutilaO <46874717+JutilaO@users.noreply.github.com>
2023-02-17 09:38:22 +0200

    Add files via upload

---
+++ Osa 4/4.8 Blogi/tests/blogs_get.test.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(2)
+  expect(response.type).toBe('application/json')
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
---
+++ Osa 4/4.9 Blogi/tests/verify_id.test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('id is defined', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    console.log(blog.id)
+    expect(blog.id).toBeDefined()
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
